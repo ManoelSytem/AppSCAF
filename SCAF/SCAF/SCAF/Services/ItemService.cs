33d204442b12da3ef399b11a0878538fa5cd9203
manoelcontatosi@gmail.com <manoelcontatosi@gmail.com>
2017-10-18 17:11:04 -0200

    SCAF -  Densenvolvimento. Incluindo novas opções no menu, criação de novas content page e TabbedPage para os menus. Foi realizando uma correção no bug android, imagem.sorce estava vazio.

--- SCAF/SCAF/SCAF/Services/ItemService.cs
+++ SCAF/SCAF/SCAF/Services/ItemService.cs
@@ -21,10 +21,16 @@ namespace SCAF.Model
             // Definimos o titulo para o item
             // o icone do lado esquerdo e a pagina que vamos abrir
             var HomeView = new MasterPageItem() { Title = "Inicio", Icon= ImageSource.FromResource("SCAF.Resource.home_icon.png"), TargetType = typeof(HomePage) };
-            var SolicitacaoCompraView = new MasterPageItem() { Title = "Solicitação de Compra", Icon = ImageSource.FromResource("SCAF.Resource.sc_icon.png"), TargetType = typeof(TabbedPageSc) };
+            var SolicitacaoCompraView = new MasterPageItem() { Title = "Solicitação de Compra", Icon = ImageSource.FromResource("SCAF.Resource.sc_icon.png"), TargetType = typeof(SolicitacaoCompraPage) };
+            var FornecedoView = new MasterPageItem() { Title = "Fornecedores", Icon = ImageSource.FromResource("SCAF.Resource.b2b.png"), TargetType = typeof(TabbedPageForn) };
+            var CotacaoView = new MasterPageItem() { Title = "Cotação", Icon = ImageSource.FromResource("SCAF.Resource.Icone_dolar.png"), TargetType = typeof(TabbedPageCotacao) };
+            var AutorizacaoFview = new MasterPageItem() { Title = "Autorização de Fornecimento", Icon = ImageSource.FromResource("SCAF.Resource.icone_forne.png"), TargetType = typeof(TabbedPageForn) };
             // Adicionando items no menuLista
             menuLista.Add(HomeView);
             menuLista.Add(SolicitacaoCompraView);
+            menuLista.Add(FornecedoView);
+            menuLista.Add(CotacaoView);
+            menuLista.Add(AutorizacaoFview);
             //retorna a lista de itens 
             return menuLista;
         }
---
+++ SCAF/SCAF/SCAF/Views/TabbedPageCotacao.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SCAF.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPageCotacao : TabbedPage
+    {
+        public TabbedPageCotacao ()
+        {
+            InitializeComponent();
+        }
+    }
+}
